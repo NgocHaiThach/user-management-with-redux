27550eddc8b257d7fec985491e92889f614bdd82
huuthe1999 <huuthe128@gmail.com>
2022-08-15 13:43:40 +0700

    Fix conflict

--- src/services/apiUsers.js
+++ src/services/apiUsers.js
@@ -23,4 +23,3 @@ export const deleteUser = id => publicRequest.delete(`/users/${id}`);
 export const addUser = data => publicRequest.post(`/users`, data);
 export const updateUser = data => publicRequest.put(`/users/${data.id}`, data);
 export const getOneUser = id => publicRequest.get(`/users/${id}`);
-export const updateUser = data => publicRequest.put(`/users/${data.id}`, data);
